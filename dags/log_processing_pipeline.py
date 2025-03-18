d61adc0f37c9e7c489d0eab1ed4d7dd43c98f857
VaibhavBansal26 <vaibhav.bansal945@gmail.com>
2025-03-18 00:20:12 -0400

    create dags for airflow

---
+++ dags/log_processing_pipeline.py
@@ -0,0 +1,150 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from confluent_kafka import Consumer,KafkaException
+from elasticsearch import Elasticsearch
+import json
+import logging
+import re
+import boto3
+from ..secret.secret import get_secret
+
+logger = logging.getLogger(__name__)
+
+
+def parse_log_entry(log_entry):
+    """
+    Function to parse a log entry
+    """
+    log_pattern = r'(?P<ip>[\d\.]+) - - \[(?P<timestamp>.*)\] "(?P<method>\w+) (?P<endpoint>[\w/]+) (?P<protocol>[\w/\.]+)'
+    match = re.match(log_pattern, log_entry)
+    if not match:
+        logger.error(f'Failed to parse log entry: {log_entry}')
+        return None
+    log = match.groupdict()
+
+    try:
+        parse_timestamp = datetime.strptime(log['timestamp'], '%d/%b/%Y:%H:%M:%S')
+        log['timestamp'] = parse_timestamp.isoformat()
+    except Exception as e:
+        logger.error(f'Failed to parse timestamp: {e}')
+        return None
+    return log
+
+def consume_and_index_logs():
+    """
+    Function to consume logs from Kafka and index them in Elasticsearch
+    """
+    # Get the secret
+    secret = get_secret('MWAA_Secrets_V2','us-east-1')
+    kafka_consumer_config = {
+        'bootstrap.servers': secret['KAFKA_BOOTSTRAP_SERVER'],
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'sasl.username': secret['KAFKA_SASL_USERNAME'],
+        'sasl.password': secret['KAFKA_SASL_PASSWORD'],
+        'group.id': 'logs-consumer',
+        'auto.offset.reset': 'latest'
+    }
+
+    # Connect to Elasticsearch
+    es_config = {
+        'hosts': secret['ELASTICSEARCH_URL'],
+        'api_key': secret['ELASTICSEARCH_API_KEY']
+    }
+
+    consumer = Consumer(kafka_consumer_config)
+    es = Elasticsearch(**es_config)
+    topic = 'website_logs'
+    consumer.subscribe([topic])
+
+    # Before we start consuming messages, we need to make sure that the index exists
+    try:
+        data_stream_index_name = 'website_logs'
+        if not es.indices.exists(data_stream_index_name):
+            es.indices.create(data_stream_index_name)
+            logger.info(f'Index created: {data_stream_index_name}')
+    except Exception as e:
+        logger.error(f'Error creating index: {e}')
+    
+
+    try:
+        logs = []
+        while True:
+            message = consumer.poll(timeout=1.0) # Poll for new messages, wait for 1 second to receive messages from Kafka
+            if message is None:
+                continue
+            if message.error():
+                if message.error().code() == KafkaException._PARTITION_EOF:
+                    logger.warning('End of partition reached {0}/{1}'
+                                    .format(message.topic(), message.partition()))
+                else:
+                    logger.error(f'Error occurred: {message.error().str()}')
+            else:
+                log_entry = message.value().decode('utf-8')
+                parsed_log = parse_log_entry(log_entry)
+
+                if parsed_log:
+                    logs.append(parsed_log)
+                
+                # index when 15000 logs are collected
+                if len(logs) >= 15000:
+                    actions = [
+                        {
+                            '_op_type': 'index',
+                            '_index': data_stream_index_name,
+                            '_source': log
+                        }
+                        for log in logs
+                        ]
+                    success, failed = es.bulk(actions, refresh=True)
+                    logger.info(f'Indexed {success} logs, failed to index {len(failed)} logs')
+                    logs = []
+    except Exception as e:
+        logger.error(f'Failed to index log: {e}')
+    
+    # index remaining logs
+    try:
+        if logs:
+            actions = [
+                {
+                    '_op_type': 'index',
+                    '_index': data_stream_index_name,
+                    '_source': log
+                }
+                for log in logs
+            ]
+            es.bulk(actions, refresh=True)
+            logs = []
+    except Exception as e:
+        logger.error(f'Failed to index log: {e}')
+    finally:
+        consumer.close()
+        es.close()
+        logger.info('Consumer closed')
+
+
+default_args = {
+    'owner': 'Data Lab',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
+}
+
+dag = DAG(
+    dag_id='log_consumer',
+    default_args=default_args,
+    description='Generate logs',
+    schedule_interval='*/5 * * * *',
+    start_date= datetime(2025, 3, 17),
+    catchup=False,
+    tags=['logs','production','kafka']
+)
+
+consume_logs_task = PythonOperator(
+    task_id='generate_and_consume_logs',
+    python_callable=consume_and_index_logs,
+    dag=dag
+)
---
+++ dags/log_producer.py
@@ -0,0 +1,120 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from confluent_kafka import Producer
+from faker import Faker
+import logging
+from datetime import datetime, timedelta
+from ..secret.secret import get_secret
+
+
+fake = Faker()
+logger = logging.getLogger(__name__)
+
+
+
+default_args = {
+    'owner': 'Data Lab',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
+}
+
+def create_kafka_producer(kafka_config):
+    """
+    Function to create a Kafka producer
+    """
+    producer = Producer(kafka_config)
+    return producer
+
+def generate_log():
+    methods = ['GET', 'POST', 'DELETE', 'PUT']
+    endpoints = ['/api/users', '/home', '/about', '/contact', '/services']
+    status_codes = [200, 201, 301, 302, 400, 404, 500]
+
+    user_agent = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.3',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.3',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.3',
+        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.3',
+    ]
+
+    # where is the traffic coming from
+    referrers = [
+        'https://example.com',
+        'https://www.google.com',
+        'https://www.bing.com',
+        'https://www.yahoo.com',
+        'https://www.duckduckgo.com',
+        'https://www.baidu.com',
+        'internal'
+    ]
+
+    ip = fake.ipv4()
+    timestamp = datetime.now().strftime('%b %d %Y, %H:%M:%S')
+    method = fake.random.choice(methods)
+    endpoints = fake.random.choice(endpoints)
+    status_code = fake.random.choice(status_codes)
+    size= fake.random_int(1000, 15000)
+    referrer = fake.random.choice(referrers)
+    user_agent = fake.random.choice(user_agent)
+
+    log_entry = f'{ip} - - [{timestamp}] "{method} {endpoints} HTTP/1.0" {status_code} {size} "{referrer}" "{user_agent}"'
+    return log_entry
+
+def delivery_report(err, msg):
+    """
+    Reports the failure or success of a message delivery.
+    """
+    if err is not None:
+        logger.error(f'Message delivery failed: {err}')
+    else:
+        logger.info(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+
+def produce_logs(**context):
+    """"    
+    Function to generate logs and produce them to Kafka topic
+    """
+    secrets = get_secret('MWAA_Secrets_V2', 'us-east-1')
+    kafka_config = {
+        'bootstrap.servers': secrets['KAFKA_BOOTSTRAP_SERVER'],
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'sasl.username': secrets['KAFKA_SASL_USERNAME'],
+        'sasl.password': secrets['KAFKA_SASL_PASSWORD'],
+        'session.timeout.ms': 50000
+    }
+
+    producer = create_kafka_producer(kafka_config)
+    topic = 'website_logs'
+
+    for _ in range(15000):
+        log = generate_log()
+        try:
+            producer.produce(topic, log.encode('utf-8'), on_delivery=delivery_report)
+            producer.flush()
+            logger.info(f'Log produced: {log}')
+        except Exception as e:
+            logger.error(f'Error producing log: {e}')
+            raise e
+        logger.info('Logs produced successfully for the {topic}')
+    
+
+dag = DAG(
+    dag_id='log_producer',
+    default_args=default_args,
+    description='Generate logs',
+    schedule_interval='*/5 * * * *',
+    start_date= datetime(2025, 3, 17),
+    catchup=False,
+    tags=['logs','production','kafka']
+)
+
+produce_logs_task = PythonOperator(
+    task_id='generate_and_produce_logs',
+    python_callable=produce_logs,
+    dag=dag
+)
